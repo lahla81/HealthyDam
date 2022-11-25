77268755339f5496f3280715335fac142921ee29
Mohamed <lahla81@yahoo.com>
2022-11-25 17:57:51 +0200

    finish categories

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
 import ProductCard from "../product-card/product-card.component";
 
 
 const CategoryPreview = ({title, products}) => {
     return(
         <div>
-            <h4 className="text-center"><span className="cursor-pointer text-bold mb-1">{title.toUpperCase()}</span></h4>
+            <h4 className="text-center">
+                <Link 
+                    className="cursor-pointer text-bold mb-1"
+                    to={title}>
+                    {title.toUpperCase()}
+                </Link>
+            </h4>
             <div className="container">
                 <div className="row mb-3">
                     {
